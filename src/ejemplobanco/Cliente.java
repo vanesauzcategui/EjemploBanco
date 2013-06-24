bec8de87b6b03a59cb576d95b4c955e0fe2fb05c
Nohelia Vanessa Ortega Alvarez <noheliavanessa@gmail.com>
2013-06-24 14:59:38 -0500

    Creacion Clase Cliente

---
+++ src/ejemplobanco/Cliente.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplobanco;
+
+/**
+ *
+ * @author Noheli@ V@ness@ Orteg@ Alvarez
+ */
+public class Cliente {
+    
+    long idCliente;
+    String nombre;
+
+    public Cliente() {
+        
+    }
+    
+    
+}
---
+++ src/ejemplobanco/EjemploBanco.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplobanco;
+
+/**
+ *
+ * @author Noheli@ V@ness@ Orteg@ Alvarez
+ */
+public class EjemploBanco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
