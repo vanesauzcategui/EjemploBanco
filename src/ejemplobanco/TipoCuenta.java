1fb6f8ea502bb0919e6c7b7aa340b7dcaba69257
Nohelia Vanessa Ortega Alvarez <noheliavanessa@gmail.com>
2013-06-24 20:32:52 -0500

    nueva clase tipo de cuenta

---
+++ src/ejemplobanco/TipoCuenta.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplobanco;
+
+/**
+ *
+ * @author Noheli@ V@ness@ Orteg@ Alvarez
+ */
+public class TipoCuenta {
+    
+}
