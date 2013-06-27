b51e272a2147179be00093543118a77876c78573
Leidy Vanesa <vanesa.uzcategui@gmail.com>
2013-06-27 09:55:12 -0500

    Update TipoOperacion.java

--- src/ejemplobanco/TipoOperacion.java
+++ src/ejemplobanco/TipoOperacion.java
@@ -1,13 +1,50 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package ejemplobanco;
+package practicabanco;
 
-/**
- *
- * @author Noheli@ V@ness@ Orteg@ Alvarez
- */
 public class TipoOperacion {
+    String descripcion;
+    int idTipoOperacion;
+
+    public TipoOperacion() {
+       
+    }
+
+    public TipoOperacion(int idTipoOperacion) {
+      this.idTipoOperacion=idTipoOperacion;
+      switch (this.idTipoOperacion) {
+      case 1:
+           this.descripcion="Retiro";
+           break;
+      case 2:
+           this.descripcion="Deposito";
+           break;
+      case 3:
+           this.descripcion="Retiro por Cajero";
+           break;
+      default:
+           this.descripcion="Otro";
+           break;
+      }
     
+    }
+    
+     public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getIdTipoOperacion() {
+        return idTipoOperacion;
+    }
+
+    public void setIdTipoOperacion(int idTipoOperacion) {
+        this.idTipoOperacion = idTipoOperacion;
+    }
+
+    @Override
+    public String toString() {
+        return "TipoOperacion{" + "descripcion=" + descripcion + ", idTipoOperacion=" + idTipoOperacion + '}';
+    }    
 }
