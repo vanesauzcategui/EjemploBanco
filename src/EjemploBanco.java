ddceb6e6bb2c1222625bfcbb3b97e33f71504780
Nohelia Vanessa Ortega Alvarez <noheliavanessa@gmail.com>
2013-06-26 20:44:53 -0500

    principal

---
+++ src/EjemploBanco.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplobanco;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Noheli@ V@ness@ Orteg@ Alvarez
+ */
+public class EjemploBanco {
+
+    /**
+     * @param args the command line arguments
+     */
+     static ArrayList<Cliente> clientes = new ArrayList<>();
+    
+    
+    public static void main(String[] args) throws IOException{
+        // TODO code application logic here
+    
+     
+     
+    }
+}
